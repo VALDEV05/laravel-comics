c722406c0baa59a0d07d72ac4cb5da6eb62702b2
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-12-21 10:11:03 +0100

    completed footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,91 @@
 <footer id="site_footer">
-    footer
+    <div class="jumbofooter">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                     <div class="row footer_menu">
+                         <div class="col-3">
+                             <h3>DC COMICS</h3>
+                             <ul>
+                                <li><a href="{{ route('characters')}}">Characters</a></li>
+                                <li><a href="{{ route('comics')}}">Comics</a></li>
+                                <li><a href="">Movie</a></li>
+                                <li><a href="">TV</a></li>
+                                <li><a href="">Games</a></li>
+                                <li><a href="">Videos</a></li>
+                                <li><a href="">News</a></li>
+                             </ul>
+                             <h3>SHOP</h3>
+                             <ul>
+                                <li><a href="">Shop DC</a></li>
+                                <li><a href="">Shop DC Collectibles</a></li>
+                             </ul>
+                         </div>
+                         <div class="col-3">
+                             <h3>DC</h3>
+                             <ul>
+                                 <li><a href="">Terms Of Use</a></li>
+                                <li><a href="">Privacy policy (New)</a></li>
+                                <li><a href="">Ad Choices</a></li>
+                                <li><a href="">Advertisting</a></li>
+                                <li><a href="">Jobs</a></li>
+                                <li><a href="">Subscriptions</a></li>
+                                <li><a href="">Talent Workshop</a></li>
+                                <li><a href="">CPSC Certificates</a></li>
+                                <li><a href="">Ratings</a></li>
+                                <li><a href="">Shop Help</a></li>
+                                <li><a href="">Contact Us</a></li>
+                             </ul>
+                         </div>
+                         <div class="col-3">
+                             <h3>SITES</h3>
+                             <ul>
+                                <li><a href="">DC</a></li>
+                                <li><a href="">MAD Magazine</a></li>
+                                <li><a href="">DC Kids</a></li>
+                                <li><a href="">DC Universe</a></li>
+                                <li><a href="">DC Power Visa</a></li>
+                             </ul>
+                         </div>
+                         <p>All Site Content TM and © 2020 DC Entertainment, unless otherwise <span><a href="">noted here.</a></span> All rights reserved.
+                            <span><a href="">Cookies Settings</a></span></p>
+                     </div>
+                </div>
+                <div class="col-6 logo-dc">
+                </div>
+            </div>
+            <!-- /footer_menu  -->
+            
+        </div>
+    </div>
+    <div id="signup">
+        <div class="container h-100 d-flex align-items-center justify-content-between" >
+            <div class="btn-signup ms-1">
+                <a href="">Sign-up now</a>
+            </div>
+            <div class="social d-flex flex-row align-items-center">
+                <h5 class="text-uppercase mt-3 me-4">FOllow us</h5>
+                <div class="socials-logo d-flex align-items-center">
+                    <div class="row ">
+                        <div class="col">
+                            <img src="{{ asset('img/footer-facebook.png')}}" alt="">
+                        </div>
+                        <div class="col">
+                            <img src="{{ asset('img/footer-twitter.png')}}" alt="">
+                        </div>
+                        <div class="col">
+                            <img src="{{ asset('img/footer-youtube.png')}}" alt="">
+                        </div>
+                        <div class="col">
+                            <img src="{{ asset('img/footer-pinterest.png')}}" alt="">
+                        </div>
+                        <div class="col">
+                            <img src="{{ asset('img/footer-periscope.png')}}" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
+<!-- /#site_footer -->
